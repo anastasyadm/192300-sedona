07dee29e88ae4d9d7ade224d8ca661144c42b2c5
Anastasia <anastasyalyashko@gmail.com>
2016-05-15 17:23:09 +0300

    анимация

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+var link = document.querySelector(".button_search");
+var form = document.querySelector(".search_form");
+var checkin = form.querySelector("[name=name_date_arr]");
+var checkout = form.querySelector("[name=name_date_dep]");
+
+link.addEventListener ("click", function(event) {
+event.preventDefault();
+form.classList.toggle("search_form_show");
+checkin.focus();
+form.classList.remove("search_form_error");
+});
+
+window.addEventListener("keydown", function(event) {
+if (event.keyCode === 27) {
+if (form.classList.contains("search_form_show")) {
+form.classList.remove("search_form_show");
+form.classList.remove("search_form_error");
+}
+}
+});
+
+form.addEventListener("submit", function(event) {
+if (!checkin.value || !checkout.value) {
+event.preventDefault();
+form.classList.add("search_form_error");
+}
+});
